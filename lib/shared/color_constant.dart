8dfadfc73f27aacb9e6ca1dd0d7429398fe4c9f7
Addin15 <addin.utb15@gmail.com>
2022-01-31 02:25:22 +0800

    first commit

---
+++ lib/shared/color_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xFF08B2E3);
+  static const Color secondaryColor = Color(0xFF020887);
+  static const Color tertiaryColor = Color(0xFF041B15);
+  static const Color lavenderGray = Color(0xFFCFCCD6);
+}
