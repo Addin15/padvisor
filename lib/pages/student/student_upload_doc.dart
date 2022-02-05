702cdda6fc6ecc3f789ea0903cf90e22e61ead59
kimcys <hakka711@gmail.com>
2022-02-05 16:38:12 +0800

    add report

--- lib/pages/student/student_upload_doc.dart
+++ lib/pages/student/student_upload_doc.dart
@@ -1,12 +1,13 @@
-import 'dart:html';
+import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:open_file/open_file.dart';
+import 'package:padvisor/pages/api/firebase_api.dart';
 import 'package:padvisor/pages/sign_in.dart';
 import 'package:padvisor/pages/student/student_feedback.dart';
 import 'package:padvisor/shared/color_constant.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 class UploadDoc extends StatefulWidget {
   const UploadDoc({Key? key}) : super(key: key);
@@ -16,8 +17,11 @@ class UploadDoc extends StatefulWidget {
 }
 
 class _UploadDocState extends State<UploadDoc> {
+  UploadTask? task;
+  File? file;
   @override
   Widget build(BuildContext context) {
+    final fileName = file != null ? basename(file!.path) : 'No File Selected';
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
@@ -61,7 +65,7 @@ class _UploadDocState extends State<UploadDoc> {
                     SizedBox(height: 100),
                     Container(
                       constraints: const BoxConstraints.tightFor(
-                          width: 400, height: 400),
+                          width: 300, height: 100),
                       padding: EdgeInsets.all(32),
                       alignment: Alignment.center,
                       child: ElevatedButton(
@@ -69,17 +73,13 @@ class _UploadDocState extends State<UploadDoc> {
                           final result = await FilePicker.platform.pickFiles();
                           if (result == null) return;
 
-                          final file = result.files.first;
-                          openFile(file);
-
-                          print('Name ${file.name}');
-                          print('Bytes ${file.bytes}');
-                          print('Size ${file.size}');
-                          print('Extension ${file.extension}');
-                          print('Path ${file.path}');
+                          final path = result.files.single.path!;
+                          setState(() {
+                            file = File(path);
+                          });
                         },
                         child: Text(
-                          'Pick File',
+                          'Select File',
                           style: TextStyle(
                             fontFamily: "Reem Kufi",
                             color: Colors.white,
@@ -89,11 +89,50 @@ class _UploadDocState extends State<UploadDoc> {
                         ),
                         style: ElevatedButton.styleFrom(
                             primary: AppColor.tertiaryColor,
-                            fixedSize: const Size(300, 100),
+                            fixedSize: const Size(400, 400),
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(10))),
                       ),
                     ),
+                    Text(
+                      fileName,
+                      style: TextStyle(
+                        fontFamily: "Reem Kufi",
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      constraints: const BoxConstraints.tightFor(
+                          width: 300, height: 100),
+                      padding: EdgeInsets.all(32),
+                      alignment: Alignment.center,
+                      child: ElevatedButton.icon(
+                        icon: Icon(Icons.cloud_upload_outlined),
+                        onPressed: () async {
+                          uploadFile();
+                        },
+                        label: Text(
+                          'Upload File',
+                          style: TextStyle(
+                            fontFamily: "Reem Kufi",
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            primary: AppColor.tertiaryColor,
+                            fixedSize: const Size(400, 400),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                    ),
+                    task != null ? buildUploadStatus(task!) : Container(),
                   ],
                 ),
               ),
@@ -104,7 +143,40 @@ class _UploadDocState extends State<UploadDoc> {
     );
   }
 
-  void openFile(PlatformFile file) {
-    OpenFile.open(file.path!);
+  Future uploadFile() async {
+    if (file == null) return;
+
+    final fileName = basename(file!.path);
+    final destination = 'files/evidence/$fileName';
+
+    task = FirebaseApi.uploadFile(destination, file!);
+    setState(() {});
+
+    if (task == null) return;
+    final snapshot = await task!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+    print('Download link: $urlDownload');
   }
+
+  Widget buildUploadStatus(UploadTask task) => StreamBuilder<TaskSnapshot>(
+        stream: task.snapshotEvents,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final snap = snapshot.data!;
+            final progress = snap.bytesTransferred / snap.totalBytes;
+            final percentage = (progress * 100);
+            return Text(
+              '$percentage%',
+              style: TextStyle(
+                fontFamily: "Reem Kufi",
+                color: Colors.black,
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+      );
 }
